42f38729c64949ed8ac9d9fba7707ec43c029256
Pawanchauhan1517 <96001846+Pawanchauhan1517@users.noreply.github.com>
2026-02-11 00:01:42 +0530

    Delete int directory

--- int/client/src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  return localStorage.getItem("token") ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
